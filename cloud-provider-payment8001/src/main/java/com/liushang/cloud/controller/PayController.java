4417d178e92dc553e474bc30d1d681e4a69bf9c6
liushang <976681935@qq.com>
2024-09-22 20:01:37 +0800

    引入consul的分布式配置中心

--- cloud-provider-payment8001/src/main/java/com/liushang/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/liushang/cloud/controller/PayController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -46,5 +47,15 @@ public class PayController {
     public ResultData<Pay> getById(@PathVariable("id") Integer id){
         return ResultData.success(payService.getById(id));
     }
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping(value = "/pay/get/info")
+    private String getInfoByConsul(@Value("${liushang.info}") String liushangInfo)
+    {
+        return "liushangInfo: "+liushangInfo+"\t"+"port: "+port;
+    }
+
 
 }
