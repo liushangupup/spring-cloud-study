f520146f78ec3af9fdc6a02b2ad9d793167dece3
liushang <976681935@qq.com>
2024-09-23 21:17:16 +0800

    consul动态及时刷新、测试默认轮询负载均衡策略、使用discoveryClient查询服务列表、替换默认的负载均衡策略为随机

--- cloud-consumer-order80/src/main/java/com/liushang/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/liushang/cloud/controller/OrderController.java
@@ -3,8 +3,12 @@ package com.liushang.cloud.controller;
 import com.liushang.cloud.entities.PayDTO;
 import com.liushang.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author liushang
@@ -27,4 +31,31 @@ public class OrderController {
     public ResultData getPayInfo(@PathVariable("id") Integer id){
         return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/"+id,ResultData.class,id);
     }
+
+    @GetMapping(value = "/consumer/pay/get/info")
+    private String getInfoByConsul()
+    {
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/info", String.class);
+    }
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+    @GetMapping("/consumer/discovery")
+    public String discovery()
+    {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+
+        System.out.println("===================================");
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId()+"\t"+element.getHost()+"\t"+element.getPort()+"\t"+element.getUri());
+        }
+
+        return instances.get(0).getServiceId()+":"+instances.get(0).getPort();
+    }
+
 }
--- cloud-provider-payment8001/src/main/java/com/liushang/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/liushang/cloud/Main8001.java
@@ -2,9 +2,11 @@ package com.liushang.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@RefreshScope
 @MapperScan("com.liushang.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
 public class Main8001 {
 
