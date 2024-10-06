55210b49b6272c7f6c23ee6f1f912c359815e627
liushang <976681935@qq.com>
2024-10-06 16:01:36 +0800

    测试CircuitBreaker断路器熔断功能（基于计数的滑动窗口）

---
+++ cloud-provider-payment8001/src/main/java/com/liushang/cloud/controller/PayCircuitController.java
@@ -0,0 +1,27 @@
+package com.liushang.cloud.controller;
+
+import cn.hutool.core.util.IdUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author liushang
+ * @date 2024-09-28 08:40:41
+ */
+@RestController
+public class PayCircuitController {
+
+    //=========Resilience4j CircuitBreaker 的例子
+    @GetMapping(value = "/pay/circuit/{id}")
+    public String myCircuit(@PathVariable("id") Integer id)
+    {
+        if(id == -4) throw new RuntimeException("----circuit id 不能负数");
+        if(id == 9999){
+            try { TimeUnit.SECONDS.sleep(5); } catch (InterruptedException e) { e.printStackTrace(); }
+        }
+        return "Hello, circuit! inputId:  "+id+" \t " + IdUtil.simpleUUID();
+    }
+}
