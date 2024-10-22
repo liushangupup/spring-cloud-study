789d9f4f6d7a53129d45fe779e47c9f9f4b8acbf
liushang <976681935@qq.com>
2024-10-22 17:26:57 +0800

    测试nacos作为配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/liushang/cloud/Main3377.java
@@ -0,0 +1,18 @@
+package com.liushang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author liushang
+ * @date 2024-10-22 12:03:24
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Main3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main3377.class,args);
+    }
+}
